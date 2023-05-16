884ea1e1a0b9b804f4dd96ca1bdc0ac465c293eb
QuiD-0 <wodnd101@naver.com>
2023-05-16 15:18:12 +0900

    init project

---
+++ src/main/kotlin/com/quid/QRcode/QRcodeApplication.kt
@@ -0,0 +1,11 @@
+package com.quid.QRcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QRcodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<QRcodeApplication>(*args)
+}
---
+++ src/test/kotlin/com/quid/QRcode/QRcodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quid.QRcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QRcodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
