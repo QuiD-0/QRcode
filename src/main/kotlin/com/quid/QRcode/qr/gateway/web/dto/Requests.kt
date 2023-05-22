53078d34874d8779c9ec815da4c9c0e736abb71f
QuiD-0 <wodnd101@naver.com>
2023-05-22 17:54:29 +0900

    qr code request

--- src/main/kotlin/com/quid/QRcode/qr/gateway/web/dto/Requests.kt
+++ src/main/kotlin/com/quid/QRcode/qr/gateway/web/dto/Requests.kt
@@ -2,11 +2,4 @@ package com.quid.QRcode.qr.gateway.web.dto
 
 data class UrlCreateRequest(
     val url: String
-){
-    init {
-        val regex = Regex("(https?://)?(www\\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&/=]*)")
-        if (!regex.matches(url)) {
-            throw IllegalArgumentException("url 형식이 올바르지 않습니다.")
-        }
-    }
-}
+)
