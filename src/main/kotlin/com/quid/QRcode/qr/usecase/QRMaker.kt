8e5daae690820a71049b77015ca9d1b6d5334154
QuiD-0 <wodnd101@naver.com>
2023-05-23 17:38:47 +0900

    add log

--- src/main/kotlin/com/quid/QRcode/qr/usecase/QRMaker.kt
+++ src/main/kotlin/com/quid/QRcode/qr/usecase/QRMaker.kt
@@ -2,6 +2,7 @@ package com.quid.QRcode.qr.usecase
 
 
 import com.quid.QRcode.qr.domain.QRCode
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import javax.servlet.ServletOutputStream
 
@@ -11,7 +12,10 @@ interface QRMaker {
 
     @Service
     class SimpleQRMaker : QRMaker {
+        private val logger = LoggerFactory.getLogger(SimpleQRMaker::class.java)
+
         override fun make(url: String, outputStream: ServletOutputStream) =
             QRCode(url).create(outputStream)
+                .also { logger.info("QRCode created for $url") }
     }
 }
